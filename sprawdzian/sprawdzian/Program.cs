4cc6fa0af805473bcd57158d29bba91fe8596b26
Camille2814 <45181529+anigamer097@users.noreply.github.com>
2024-04-09 15:35:20 +0200

    tablice

--- sprawdzian/sprawdzian/Program.cs
+++ sprawdzian/sprawdzian/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class sprawdzian
+{
+    static void Main(String[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Szukanie napisów: ");
+        Tab();
+
+    }
+    static void Tab()
+    {
+        string[] tab = { "stray kids", "kpop", "bangchan", "lee know", "changbin", "hyunjin", "han jisung", "felix", "seungmin", "I.N" };
+        Console.Write("Jakiego słowa szukasz?: ");
+        string sz = Console.ReadLine();
+        bool tak = false;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            if (sz == tab[i])
+            {
+                Console.Write(tab[i] + " jest w tablicy");
+                tak = true;
+            }
+        }
+    }
+}
